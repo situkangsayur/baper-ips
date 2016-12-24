1fef320de839bf448bb0893d6964a6ebb8bc9d82
Hendri Karisma <situkangsayur@gmail.com>
2016-12-24 12:38:14 +0700

    clean println

--- app/model/normalization.py
+++ app/model/normalization.py
@@ -25,13 +25,12 @@ class Normalization():
 
     def get_normalize_single_data(self, data):
         temp_array = []
+
         for temp_field in data:
             print(temp_field)
             temp_values = dataset[self.COLLECTION_NAME].find_one({'field': temp_field}, {'_id': 0})
             temp_value = list(filter(lambda x: x['value'] == data[temp_field], temp_values['values']))
             temp_array.append(temp_value[0]['norm'])
-
-        print(temp_array)
 
         return temp_array
 
@@ -54,18 +53,14 @@ class Normalization():
             if(is_label==False):
 
                 for temp_field in temp_feature:
-                    # print(temp)
                     temp_values = dataset[self.COLLECTION_NAME].find_one({'field': temp_field}, {'_id': 0})
                     temp_value = list(filter(lambda x: x['value'] == temp[temp_field], temp_values['values']))
                     temp_array.append(temp_value[0]['norm'])
 
-                print(temp_array)
                 return temp_array
             else:
                 temp_values = dataset[self.COLLECTION_NAME].find_one({'field': temp_feature}, {'_id': 0})
-
                 temp_value = list(filter(lambda x: x['value'] == temp[temp_feature], temp_values['values']))
-                #     print(p)
                 temp_array.append(temp_value[0]['id'])
 
                 return temp_array
@@ -75,28 +70,16 @@ class Normalization():
     def generate_normalizaiton(self):
         self.load_dataset()
         features = dataset[self.collection].find_one({}, {'_id': 0})
-        # print('val' + features)
         temp_feature = list(map(lambda x: x, features))
 
-        # for t in temp_feature:
-        #     print(t)
         prior_knowledge = {};
-        # def add_prio_knowledge(x,i) :
-        #     data['id']
         temp_dataset = list(dataset[self.collection].find({}, {'_id': 0}))
         for temp in temp_feature:
 
             temp_knowledge = list(set(map(lambda x: x[temp], temp_dataset)))
             range_val = list(range(0, len(temp_knowledge)))
-
             zipped = zip(range_val, temp_knowledge)
-            # print('zipp count :  ' + len(zipped))
-            # for temp in zipped:
-            #     print(temp)
 
-            print(temp)
-            print(min(range_val))
-            print(max(range_val))
             tupple = list(
                 map(lambda x, y: {'id': x, 'value': y, 'norm': self.normal_value(x, min(range_val), max(range_val))}, range_val,
                     temp_knowledge))
